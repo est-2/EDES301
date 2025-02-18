37e6f000c95dea8f6d6d274d2262ef1d0a33f6b1
Demo User <debian@beaglebone.localdomain>
2025-02-18 17:16:22 +0000

    cleaned up

--- python/blink_led/blink_USR3.py
+++ python/blink_led/blink_USR3.py
@@ -39,21 +39,9 @@ This code will blink the USR3 LED on PocketBeagle at 5 Hz.
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-#for i in range(4):
- #   GPIO.setup("USR%d" % i, GPIO.OUT)
-
-#while True:
- #   for i in range(4):
-  #      GPIO.output("USR%d" % i, GPIO.HIGH)
-   #     time.sleep(1)
-#    for i in range(4):
- #       GPIO.output("USR%d" % i, GPIO.LOW)
-  #      time.sleep(1)
-
 GPIO.setup("USR3",GPIO.OUT)     
 while True:
     GPIO.output("USR3", GPIO.HIGH)
     time.sleep(0.100)
     GPIO.output("USR3", GPIO.LOW)
     time.sleep(0.100)
-
