805f969732e2748e294f164f5e427ad7fe51b501
Demo User <debian@beaglebone.localdomain>
2025-04-25 00:12:33 +0000

    added set study time feature

--- project_01/prism_pomodoro.py
+++ project_01/prism_pomodoro.py
@@ -48,11 +48,14 @@ Uses:
 import time
 import board
 
+import digitalio
+
 import adafruit_character_lcd.character_lcd as characterlcd
 
 import LED_matrix    as MATRIX
 import button        as BUTTON
 import servo         as SERVO
+import LCD_16x2      as LCD_16x2
 
 
 # ------------------------------------------------------------------------
@@ -87,15 +90,20 @@ class PrismPomodoro():
     lcd_d4          = None
     
     def __init__(self, black_button="P2_2", blue_button="P2_4", yellow_button="P2_6",
-                       servo="P1_36", cs=board.P1_6, spi=board.SPI()):
+                       servo="P1_36", cs=board.P1_6, spi=board.SPI(),
+                       lcd_rs=board.P2_33, lcd_en=board.P2_35, lcd_d7=board.P2_31, lcd_d6=board.P2_29, lcd_d5=board.P2_27, lcd_d4=board.P2_25,
+                       study_time=0, break_time=0):
         """ Initialize variables and set up display """
 
         self.black_button       = BUTTON.Button(black_button)
         self.blue_button        = BUTTON.Button(blue_button)
         self.yellow_button      = BUTTON.Button(yellow_button)
-        self.servo              = SERVO.Servo(servo, default_position=SERVO_LOCK)
-        self.LED_matrix         = MATRIX.LEDMatrix(cs, spi)
+#        self.servo              = SERVO.Servo(servo, default_position=SERVO_LOCK)
+        self.LED_matrix         = MATRIX.LEDMatrix(digitalio.DigitalInOut(cs), spi)
+        self.LCD_screen         = LCD_16x2.LCD(lcd_rs, lcd_en, lcd_d7, lcd_d6, lcd_d5, lcd_d4)
         
+        self.study_time = study_time
+        self.break_time = break_time
         
         self._setup()
     
@@ -104,13 +112,25 @@ class PrismPomodoro():
     
     def _setup(self):
         """Setup the hardware components."""
-
+        self.LCD_screen.clear_screen()
         # Buttons / Servo / 
         #   - All initialized by libraries when instanitated
 
     # End def
 
-
+    def set_study_time(self):
+        """Set the study time"""
+        self.LCD_screen.write_in_position(0, 12, ",  '")
+        self.LCD_screen.write_in_position(1, 0, "Set study time")
+        while(self.black_button.is_pressed() == False):
+            print("ready for buttons")
+            if (self.yellow_button.is_pressed() == True):
+                self.study_time = self.study_time + 5
+                print("yellow pressed")
+                self.LCD_screen.write(format(self.study_time))
+            if (self.blue_button.is_pressed() == True):
+                self.study_time = self.study_time - 5
+                print("blue pressed")
 
 # End class
 
@@ -125,15 +145,17 @@ if __name__ == '__main__':
     print("Program Start")
 
     # Create instantiation of the lock
-    combo_lock = CombinationLock(debug=False)
+    prism_pomodoro = PrismPomodoro()
 
     try:
         # Run the lock
-        combo_lock.run()
+       # combo_lock.run()
+       prism_pomodoro.set_study_time()
 
     except KeyboardInterrupt:
         # Clean up hardware when exiting
-        combo_lock.cleanup()
+       # combo_lock.cleanup()
+       pass
 
     print("Program Complete")
 
